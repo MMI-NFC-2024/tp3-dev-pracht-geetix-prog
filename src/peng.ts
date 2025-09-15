78e96ff30c14d471002cdabe81f6285c47b650a3
Guellati <mathis.guellati@edu.univ-fcomte.fr>
2025-09-15 13:56:06 +0200

    init-tp

---
+++ src/peng.ts
@@ -0,0 +1,9 @@
+export interface Penguins {
+    species: string
+    island: string
+    culmen_length_mm?: number
+    culmen_depth_mm?: number
+    flipper_length_mm?: number
+    body_mass_g?: number
+    sex?: string
+}
